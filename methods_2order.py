e090a69dd6366f5f6695223f3193f01d4c96a35c
Arturo Suárez <manuel.suarez@cimat.mx>
2022-12-06 08:07:14 -0600

    Implementación base del algoritmo MB-LSR1

---
+++ methods_2order.py
@@ -0,0 +1,260 @@
+import numpy as np
+
+def MB_LSR1(theta=[], fun=None, grad=None, gd_params={}, f_params={}):
+    '''
+    Stochastic SR1 Trust-region scheme (MB-LSR1)
+    Griffin, et. al.
+    A minibatch stochastic Quasi-Newton method adapted for nonconvex deep learning problems
+    Algorithm 1, p. 5
+
+    Parámetros
+    -----------
+    theta     :   condicion inicial
+    fun       :   función de costo
+    grad      :   gradiente de la función de costo
+    gd_params :   lista de parametros para el algoritmo de descenso,
+                    nIter    = gd_params['nIter'] número de iteraciones
+                    X,y:                Observations and labels of the data to classify
+                    seed:               Random seeder
+                    numIter:            Max. num. of iterations
+                    mmr:                Memory size
+                    r_mmr:              Restart memory size r_mmr <= m
+                    n:                  Initial batch size
+                    tau:                Restart tolerance
+                    K:                  Progress check frequency
+                    gamma_1, gamma2:    Progress threshold parameters
+                    zeta:               Progressive radius parameter [0, 1]
+                    mu:                 Momentum
+                    alpha_s:            Learning rate [0, 1]
+                    radius:             Radius of the sampling region
+                    eps:                Epsilon criterion to sample S,Y pairs
+                    eta:                Trust region increase/reduce criterion
+                    delta_init:         Initial radius of trust region
+                    epsTR:              Epsilon criterion for trust region subproblem
+    f_params  :   lista de parametros para la funcion objetivo,
+                      kappa = f_params['kappa'] parametro de escala (rechazo de outliers)
+                      X     = f_params['X'] Variable independiente
+                      y     = f_params['y'] Variable dependiente
+
+    Regresa
+    -----------
+    Theta     :   trayectoria de los parametros
+                     Theta[-1] es el valor alcanzado en la ultima iteracion
+    '''
+    epsilon = 1e-8
+    # Line 1.- Input
+    N       = gd_params['N']        # Num iterations
+    mmr     = gd_params['mmr']      # Memory
+    mmr_h   = gd_params['mmr_h']    # Restart memory
+    n       = gd_params['n']        # Initial batch-size
+    delta_0 = gd_params['delta_0']  # Initial radius (delta_0)
+    # Line 2.- Parameters
+    tau     = gd_params['tau']      # Restart tolerance
+    K       = gd_params['K']        # Progress check frequency
+    gamma_1 = gd_params['gamma_1']  # Progress threshold parameter 1
+    gamma_2 = gd_params['gamma_2']  # Progress threshold parameter 2
+    nh_k    = gd_params['nh_k']     # Progress check batch-size
+    zeta    = gd_params['zeta']     # Progressive radius parameter [0, 1]
+    eta     = gd_params['eta']      # Step parameter
+    mu      = gd_params['mu']       # Momentum
+    alpha_s = gd_params['alpha_s']  # Learning-rate [0, 1]
+    # Parámetros adicionales
+    eps     = gd_params['eps']      # Termination Check Tolerance
+
+    # Line 3.- Variables
+    v = 0.0
+    T = 0
+    k = 0
+    rho_h = 0
+    s = y = v = 0
+    S = Y = []
+    Theta = []
+    # Line 4
+    for t in range(N):
+        # Line 5
+        # p = np.zeros(theta.shape)
+        s, y, v, rho, R = getStep(
+            # Parámetros del algoritmo
+            s, v, delta, eta, mu, alpha_s,
+            # Parámetros de la función objetivo y gradiente
+            fun, grad, theta, f_params
+        )
+        # Line 6
+        thetaNew = theta + s
+
+        # Line 7
+        if (exitCheck(grad, theta, f_params, eps) == 1): break
+        # Line 8
+        delta, rho_k, T = getRadius(delta, s, rho, zeta, rho_k, T)
+        # Line 9
+        S, Y = updateSY(S, Y, s, y, rho, tau, mmr_h)
+        # Line 10
+        n, fK, zeta = getBatch(n, N, fK, k, K, R, gamma_1, gamma_2, zeta, fun, grad, theta, f_params)
+        # Update variables
+        theta = thetaNew
+        Theta.append(theta)
+        k = k + 1
+    return np.array(Theta)
+
+# Algorithm 4 Search direction computation algorithm
+# Griffin, et. al., p. 6
+def getStep(
+        # Parámetros del algoritmo
+        s, v, delta, eta, mu, alpha_s,
+        # Parámetros adicionales del algoritmo
+        Eps, S, Y,
+        # Parámetros de la función objetivo y gradiente
+        fun, grad, theta, f_params):
+    '''
+        Parameters:
+            s: vector
+            v: vector
+            delta: radius
+            eta: threshold
+            mu: momentum
+            alpha_s: learning rate
+            eps: epsilon
+            grad_k: batch gradient
+            S,Y: curvature pairs
+    '''
+    # Line 2.- Compute batch gradient gk = \nabla f(wk)
+    gk = grad(theta, f_params)
+    # Line 3.- Compute p* by applying the algorithm proposed in [8] using latest S, Y pairs
+    # TODO Implement calculation of p*
+    ps = calculate_pstar(S,Y)
+    # Line 4
+    v = mu * v - eta * alpha_s * gk + (1 - eta)*s
+    # Line 5
+    v = min(1, delta/np.linalg.norm(v))*v
+    # Line 6
+    p = (1 - eta)*ps + mu * v
+    # Line 7
+    p = min(1, delta/np.linalg.norm(p))*p
+    # Line 8
+    if p.T.dot(gk) > 0:
+        # Line 9
+        p = -p
+    gkTp = gk.T.dot(p)
+    # Line 11
+    if min(np.abs(gkTp),np.abs(gkTp)/(np.linalg.norm(p)**2)) < Eps*(np.linalg.norm(gk)**2):
+        # Line 12.- restart LSR1 with initial matrix being I
+        # Line 13.- return and restart getStep
+        return
+    # Line 15.- Get step-size alpha from line-search on current batch
+    alpha = 1
+    c1 = 1e-4
+    rho_ls = 0.5
+    # Line search on Armijo condition f(wk + alpha*p) <= f(wk) + c1*alpha*( grad(wk).T @ p)
+    while fun(theta + alpha * p, f_params) > fun(theta, f_params) + c1 * alpha * (gk.T.dot(p)):
+        alpha = rho_ls * alpha
+    # Line 16
+    s = alpha * p
+    # Line 17
+    # Compute actual reduction
+    R = fun(theta + ps, f_params) - fun(theta, f_params)
+    # Compute predicted reduction
+    Lp = np.zeros((Y.shape[1], Y.shape[1]))
+    for ii in range(Y.shape[1]):
+        for jj in range(0, ii):
+            Lp[ii, jj] = S[:, ii].dot(Y[:, jj])
+    tmpp = np.sum((S * Y), axis=0)
+    Dp = np.diag(tmpp)
+    Mp = (Dp + Lp + Lp.T)
+    Minvp = np.linalg.inv(Mp)
+    tmpp1 = np.matmul(Y.T, ps)
+    tmpp2 = np.matmul(Minvp, tmpp1)
+    Bk_ps = np.matmul(Y, tmpp2)
+    Q = -(gk.T.dot(ps) + 0.5 * ps.T.dot(Bk_ps))  # Compute predicted reduction
+    # Line 18
+    rho = R / Q
+    # Line 19
+    y = grad(theta + s, f_params) - gk
+    return s, y, v, rho, R
+
+# Algorithm 2 Termination Check
+# Griffin, et. al., p. 5
+def exitCheck(grad, theta, f_params, eps):
+    # Line 3
+    # TODO Implement batch gradient
+    if np.linalg.norm(grad(theta, f_params)) <= eps:
+        # Line 4, 5.- Evaluate new gk over whole data set
+        if np.linalg.norm(grad(theta, f_params)) <= eps:
+            # Line 6: Stop
+            return 1
+    # Line 9: Not stop
+    return 0
+
+# Algorithm 5 Example update radius function
+# Griffin, et. al. p. 7
+def getRadius(delta, s, rho, zeta, rho_h, T):
+    # Line 2.- update non-monotone ratio threshold rho_h
+    # Line 3
+    rho_h = zeta * T * rho_h + rho
+    # Line 4
+    T = zeta * T + 1
+    # Line 5
+    rho_h = rho_h / T
+    ns = np.linalg.norm(s)
+    # Line 6
+    if rho_h < 0.1:
+        # Line 7
+        delta = min(delta, ns)
+    else:
+        if rho_h >= 0.5 and ns >= delta:
+            delta = 2 * delta
+    return delta, rho_h, T
+
+# Algorithm 3 Pair selection algorithm
+# Griffin, et. al. p. 6
+def updateSY(S, Y, s, y, rho, tau, k, mmr, mmr_h):
+    # Line 2
+    if rho < tau and mmr_h >= 0:
+        if mmr_h == mmr:
+            # Line 4.- Warm start according to [1]
+            # Line 5: Generate m pairs and store in S and Y
+            # TODO Implement generation of m pairs
+            pass
+        # Line 6
+        else:
+            # Line 6
+            if mmr_h == 0:
+                # Line 7
+                S = []
+                Y = []
+                # Restart
+    else:
+        # Classic S, Y update
+        # Line 11
+        S.append(s)
+        Y.append(y)
+        # Line 12
+        if k > mmr:
+            S.pop(0)
+            Y.pop(0)
+    # Line 14
+    return S,Y
+
+# Algorithm 6 Example batch-size correction function
+# Griffin, et. al. p. 7
+def getBatch(nb, N, fK, k, K, R, gamma_1, gamma_2, zeta, fun, grad, theta, f_params):
+    # Line 2
+    if K % k == 0:
+        # Line 3.- Randomly draw new progress check sample of size nh = min(1.1*nb, 5000)
+        # Line 4.- Define f using the new sample
+        # Line 5
+        sumRj = 0
+        for j in range(K-k,k):
+            sumRj = sumRj + R[j]
+        if grad(theta, f_params) - fK >= -gamma_1 + gamma_2 * sumRj:
+            # Line 6.- Progress worse than simple line-search
+            # Line 7
+            nb = min(2*nb, N)
+            # Line 8
+            if nb == N:
+                # Line 9
+                zeta = 0
+        # Line 12.- Update new target bound fK
+        # Line 13
+        fK = fun(theta, f_params)
+    # Line 15.- Randomly draw batch Ik+1 with size nb defining f^
+    return nb, fK, zeta
