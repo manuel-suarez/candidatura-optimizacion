b1e6f5e8ab9b070c13711a3eaee4a1d493bdb6c4
Arturo Suárez <manuel.suarez@cimat.mx>
2022-12-08 01:44:10 -0600

    Implementación de métodos phi

---
+++ phi.jl
@@ -0,0 +1,17 @@
+function phi(σ, Δ, a, λ)
+    obs_tol = 1e-10
+    t = λ + σ
+    if (sum(abs.(a) .< obs_tol) > 0) || (sum(abs(t) < obs_tol) > 0)
+        llpll2 = 0
+        for i in 1:length(a)
+            if (abs(a[i]) > obs_tol) && (abs(t[i]) > obs_tol)
+                return -1/Δ
+            elseif (abs(a[i]) > obs_tol) && (abs(t[i]) > obs_tol)
+                llpll2 = llpll2 + (a[i]/t[i])^2
+            end
+        end
+        return 1/sqrt(llpll2) - 1/Δ
+    end
+    llpll = norm(a./t)
+    phi = 1/llpll - 1/delta
+end
---
+++ phi_phiprim.jl
@@ -0,0 +1,24 @@
+function phi_phiprim(σ, Δ, a, λ)
+    obs_tol = 1e-10
+    t = λ + σ
+    if (sum(abs.(a) .< obs_tol) > 0) || (sum(abs(t) < obs_tol) > 0)
+        llpll2 = 0
+        llpll_prim = 0
+        for i in 1:length(a)
+            if (abs(a[i]) > obs_tol) && (abs(t[i]) > obs_tol)
+                return -1/Δ, 1/obs_tol
+            elseif (abs(a[i]) > obs_tol) && (abs(t[i]) > obs_tol)
+                llpll2 = llpll2 + (a[i]/t[i])^2
+                llpll_prim = llpll_prim + (a[i]^2/t[i]^3)
+            end
+        end
+        llpll = sqrt(llpll2)
+        phi = 1/llpll - 1/delta
+        phi_prim = llpll_prim / llpll^3
+        return phi, phi_phiprim
+    end
+    llpll = norm(a./t)
+    phi = 1/llpll - 1/delta
+    phi_prim = sum(a.^2 ./ t.^3) / llpll^3
+    return phi, phi_phiprim
+end
