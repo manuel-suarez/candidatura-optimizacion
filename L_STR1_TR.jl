093919e93fd75941a0fdbdaa3cfb183a6e0e3ea3
Arturo Suárez <manuel.suarez@cimat.mx>
2022-12-07 22:43:28 -0600

    Use of unicode characters

--- L_STR1_TR.jl
+++ L_STR1_TR.jl
@@ -1,9 +1,11 @@
+# Definición de librerías
+using LinearAlgebra
 # Definición de la función
-function func(theta, X, y)
+function func(w, X, y)
     
 end
 # Definición del gradiente
-function grad(theta, X, y)
+function grad(w, X, y)
     
 end
 
@@ -11,8 +13,8 @@ S           = [];
 Y           = [];
 
 tol         = 1e-5;
-delta       = 1;
-gamma       = 1;
+δ           = 1;
+γ           = 1;
 
 epoch       = 10;
 lim_m       = 20;
@@ -25,10 +27,37 @@ epoch_k     = 0;
 X           = 0
 y           = 0
 # Definición del vector de entrada (generado aleatoriamente)
-theta       = [1,1]
+w           = [1,1]
 while true
     println("=======================> Iteración $(k) <===================");
-    f = func(theta, X, y)
+    f           = func(w, X, y)
+    g           = grad(w, X, y)
+    llgll       = norm(g)
+
+    # Trust-Region subproblem
+    if k == 0 || size(S,2) == 0
+        p       = -δ*(g/llgll)
+        Bp      =  γ*p
+    else
+        p       = TRsubproblem_solver_OBS(δ, γ, g, Ψ, Minv)
+        Bp      = γ*p + Ψ*(Minv\(Ψ'*p))
+    end
+
+    Q_p         = p'*(g + 0.5*Bp)
+    llpll       = norm(p)
+
+    # Actualización del vector de pesos
+    w_new       = w + p
+    f_new       = func(w_new, X, y)
+    g_new       = grad(w_new, X, y)
+
+    # Vectores de curvatura (s,y)
+    s           = p
+    y           = g_new - g
+
+    # Reduction ratio
+    ρ           = (f_new - f) / Q_p
+
 
     global k = k + 1;
 end
