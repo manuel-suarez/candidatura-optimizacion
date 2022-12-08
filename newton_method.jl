763e547684a76ff50f456caedc9f37b8302a1411
Arturo Suárez <manuel.suarez@cimat.mx>
2022-12-08 01:47:25 -0600

    Implementación del método de Newton

---
+++ newton_method.jl
@@ -0,0 +1,13 @@
+function newton_method(σ_0, Δ, a, λ)
+    newton_tol      = 1e-10
+    newton_maxit    = 100
+
+    k               = 0
+    σ               = σ_0
+    ϕ, ϕ_prim       = phi_phiprim(σ, Δ, a, λ)
+    while (abs(ϕ)>newton_tol) && (k < newton_maxit)
+        σ = σ - ϕ/ϕ_prim
+        ϕ, ϕ_prim = phi_phiprim(σ, Δ, a, λ)
+        k = k + 1
+    end
+end
