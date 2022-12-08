8d79ec4cd53f88f738d78d31fda96cbe0bad640a
Arturo Suárez <manuel.suarez@cimat.mx>
2022-12-07 23:24:13 -0600

    Finalizamos implementación de algoritmo principal (S-LSR1-TR)

--- L_STR1_TR.jl
+++ L_STR1_TR.jl
@@ -82,7 +82,46 @@ while true
     end
     Δ = Δ_new
 
-    
+    # Condición de Actualización
+    y_Bs        = y - Bp
+    if abs(s'*y_Bs) > 1e-8*llpll*norm(y_Bs)
+        if k == 0
+            S = s
+            Y = y
+        else
+            S = [S s]
+            Y = [Y y]
+        end
+        if (size(S,2) > lim_m)
+            S = S[:, 2:end]
+            Y = Y[:, 2:end]
+        end
+
+        while (size(S, 2) > 0)
+            println("Curvature pairs")
+            SY      = S'*Y
+            SS      = S'*S
+            LDLt    = tril(SY) + tril(SY,-1)'
+            eig_val = eigen(LDLt,SS).values
+            λHat_min= minimum(eig_val)
+            if λHat_min > 0
+                γ   = max(0.5*λHat_min, 1e-6)
+            else
+                γ   = min(1.5*λHat_min, -1e-6)
+            end
+
+            Minv    = (LDLt - γ*SS)
+            Psi     = Y - gamma*S
+
+            if size(Psi,2) == rank(Psi) && rank(Minv) == size(Minv,2)
+                break
+            else
+                println("Psi is NOT full column rank! or M is not invertible!")
+                S = S[:, 2:end]
+                Y = Y[:, 2:end]
+            end
+        end
+    end
 
     global k = k + 1;
 end
